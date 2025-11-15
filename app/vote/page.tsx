456c9116f617ee211cfc1e631b7acd8e024d32e0
백승선 <baekjimmy@yonsei.ac.kr>
2025-11-15 17:06:51 +0900

    [#4]refactor: 애니메이션 수정

--- app/vote/page.tsx
+++ app/vote/page.tsx
@@ -3,12 +3,13 @@ import VoteMainHeader from "@/components/voteMainHeader";
 import VoteBody from "@/components/voteBody";
 
 export default function Vote() {
-    return (
-        <main className="h-[315px] w-[169px] flex flex-col bg-[#FFD954] rounded-[19px]">
-            <section className="flex flex-1 flex-col items-center">
-                <VoteMainHeader />
-                <VoteBody />
-            </section>
-        </main>
-    );
+  return (
+
+    <div className="relative w-full h-screen flex flex-col bg-[#FFD954]">
+      <section className="flex flex-1 flex-col items-center">
+        <VoteMainHeader />
+        <VoteBody />
+      </section>
+    </div>
+  );
 }
