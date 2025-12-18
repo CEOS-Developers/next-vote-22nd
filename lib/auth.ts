2793f17dee3f820c6ac44ff04a26b07589d71266
sungahChooo <170587664+sungahChooo@users.noreply.github.com>
2025-12-19 03:31:50 +0900

    fix: 로그인 유효성 검사 버그 해결

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,10 +4,7 @@ export const loginSchema = z.object({
     .string()
     .min(1, '아이디를 입력해주세요')
     .regex(/^[a-zA-Z0-9]+$/, '아이디는 영문과 숫자만 사용할 수 있습니다'),
-  password: z
-    .string()
-    .min(1, '비밀번호를 입력해주세요')
-    .regex(/^[a-zA-Z0-9]+$/, '비밀번호는 영문과 숫자만 사용할 수 있습니다'),
+  password: z.string().min(1, '비밀번호를 입력해주세요'),
 });
 
 const idSchema = z
