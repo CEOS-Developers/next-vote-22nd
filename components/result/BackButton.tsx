18a13b9d53e5e5ab6f6d90ca1d783fa9e7fa5b99
백승선 <baekjimmy@yonsei.ac.kr>
2025-12-20 20:51:29 +0900

    refactor: 로그인/투표 api 오류 해결

--- components/result/BackButton.tsx
+++ components/result/BackButton.tsx
@@ -6,7 +6,7 @@ export default function BackButton() {
 
   return (
     <Link
-      href={'/auth/select'}
+      href={'/select'}
       className="w-[150px] h-[40px] rounded-full bg-[#D9D9D9] border-[2.1px] border-black text-[14px]
       text-black font-bold flex items-center justify-center hover:bg-yellow-300 mt-[40px]"
     >
--- lib/api/token.ts
+++ lib/api/token.ts
@@ -1,5 +1,24 @@
-//로그인 api연결 전 임시
+
+const ZUSTAND_AUTH_KEY = "accessToken";
+type PersistedAuth = {
+  state?: {
+    accessToken?: string | null;
+  };
+};
+
 export function getAccessToken(): string {
-  return "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmNzNmNDJjNy04ZWUzLTQ0MGItYTNhOC1hMTczMjYxZTU3NDkiLCJyb2xlIjoiUk9MRV9VU0" +
-    "VSIiwiaWF0IjoxNzY2MjIzNDM0LCJleHAiOjE3NjYyMjcwMzR9.wKsX1s74MeCWpErKt5tpKhdVTPE1a4qv-p8bf5pBBFQ";
+  if (typeof window === "undefined") return "";
+
+  const raw = localStorage.getItem(ZUSTAND_AUTH_KEY);
+  if (!raw) return "";
+
+  // zustand persist json으로 저장되게
+  try {
+    const parsed = JSON.parse(raw) as PersistedAuth;
+    return parsed?.state?.accessToken ?? "";
+  } catch {
+    return raw;
+  }
 }
+
+
